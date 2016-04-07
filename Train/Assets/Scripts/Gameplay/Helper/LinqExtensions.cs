e7fd328022e715e47b2910935cfef0bcf896782b
Conrado Adevany Clarke <conrado.clarke@cnova.com>
2016-04-07 14:36:30 -0300

    Adding Left-Aligned Multiline text and Hyphenated Multiline, and restricting characters according to the size of the rect.

---
+++ Train/Assets/Scripts/Gameplay/Helper/LinqExtensions.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public static class LinqExtensions
+{
+    public static IEnumerable<IEnumerable<T>> Batch<T>(this IEnumerable<T> items,
+                                                  int maxItems)
+    {
+        return items.Select((item, inx) => new { item, inx })
+                    .GroupBy(x => x.inx / maxItems)
+                    .Select(g => g.Select(x => x.item));
+    }
+
+    public static IEnumerable<IEnumerable<char>> HyphenateAllLines(this IEnumerable<char> chars, int charsPerLine)
+    {
+        return chars.Hyphenate(charsPerLine).ToArray().Batch(charsPerLine);
+    }
+
+    public static IEnumerable<char> Hyphenate(this IEnumerable<char> chars, int charsPerLine)
+    {
+        int currentCharCount = 1;
+        int currentIndex = 0;
+        var enumerator = chars.GetEnumerator();
+        char previous = '\0';
+        char[] copy = chars.ToArray();
+
+        while (enumerator.MoveNext())
+        {
+            char current = enumerator.Current;
+            if ((currentCharCount == 0 || currentCharCount % charsPerLine == 1) && current == ' ')
+            {
+                currentIndex++;
+                Debug.Log(string.Format("Space at first char - current:'{0}' | currentCharCount:'{1}' | charsPerLine:'{2}'", current, currentCharCount, charsPerLine));
+                continue;
+            }
+
+            if (currentCharCount % charsPerLine == charsPerLine - 1)
+            {
+                char next = copy.Skip(currentIndex + 1).FirstOrDefault();
+                char next2 = copy.Skip(currentIndex + 2).FirstOrDefault();
+
+                if (next == default(char))
+                {
+                    yield return current;
+                    yield break;
+                }
+
+                if (next2 == default(char))
+                {
+                    yield return current;
+                    yield return next;
+                    yield break;
+                }
+
+                if (current != ' ' && previous != ' ' && next != ' ' && next2 != ' ')
+                {
+                    currentCharCount++;
+                    Debug.Log(string.Format("Hyphenating - current:'{0}' | currentCharCount:'{1}' | charsPerLine:'{2}'", current.ToString() + next + next2, currentCharCount, charsPerLine));
+                    yield return current;
+                    if (charsPerLine > 1)
+                    {
+                        yield return '-';
+                    }
+                }
+                else
+                {
+                    Debug.Log(string.Format("Not Hyphenating - current:'{0}' | currentCharCount:'{1}' | charsPerLine:'{2}'", current.ToString() + next + next2, currentCharCount, charsPerLine));
+                    yield return current;
+                }
+            }
+            else
+            {
+                Debug.Log(string.Format("Placing char - current:'{0}' | currentCharCount:'{1}' | charsPerLine:'{2}'", current, currentCharCount, charsPerLine));
+                yield return current;
+            }
+            previous = current;
+            currentCharCount++;
+            currentIndex++;
+        }
+    }
+
+    public static IEnumerable<IEnumerable<char>> GroupByWords(this IEnumerable<char> chars, int charsPerLine)
+    {
+        char[] copy = chars.ToArray();
+
+        Queue<char> allChars = new Queue<char>(chars);
+
+        while (allChars.Count > 0)
+        {
+            var firstSpaces = allChars.TakeWhile(c => c == ' ').ToList();
+            var charsInLine = allChars.SkipWhile(c => c == ' ').Take(charsPerLine).ToList();
+            var fittingChars = charsInLine.TakeWhile((c, i) => i < charsInLine.LastIndexOf(' ')).ToList();
+
+            Debug.Log("charsInLine: " + new string(charsInLine.ToArray()));
+            Debug.Log("fittingChars: " + new string(fittingChars.ToArray()));
+
+            if (allChars.Count + fittingChars.Count <= charsPerLine)
+            {
+                for (int i=0;i< charsInLine.Count+1; i++)
+                {
+                    if (allChars.Count>0)
+                    allChars.Dequeue();
+                }
+                yield return charsInLine;
+                continue;
+            }
+
+            firstSpaces.ForEach(c => allChars.Dequeue());
+            fittingChars.ForEach(c => allChars.Dequeue());
+            if (fittingChars.Count == 0)
+            {
+                var charsInLineExceptLast = charsInLine.TakeWhile((c, i) => i < charsInLine.Count - 1).ToList();
+                charsInLineExceptLast.ForEach(c => allChars.Dequeue());
+                if (allChars.Count <= 1)
+                {
+                    yield return charsInLineExceptLast;
+                    yield return allChars;
+                    break;
+                }
+                if (allChars.Count > 0)
+                {
+                    yield return charsInLineExceptLast.Concat(new char[] { '-' });
+                }
+                continue;
+            }
+           
+            yield return fittingChars;
+        }
+    }
+
+    //public static IEnumerable<IEnumerable<char>> JustifyAllLines(this IEnumerable<char> chars, int charsPerLine)
+    //{
+    //    char[] copy = chars.ToArray();
+
+    //    Queue<char> allChars = new Queue<char>(chars);
+
+    //    while (allChars.Count > 0)
+    //    {
+    //        var firstSpaces = allChars.TakeWhile(c => c == ' ').ToList();
+    //        var charsInLine = allChars.SkipWhile(c => c == ' ').Take(charsPerLine).ToList();
+    //        var fittingChars = charsInLine.TakeWhile((c, i) => i < charsInLine.LastIndexOf(' ')).ToList();
+
+    //        firstSpaces.ForEach(c => allChars.Dequeue());
+    //        fittingChars.ForEach(c => allChars.Dequeue());
+
+    //        int numberOfSpaces = fittingChars.Count(c => c == ' ');
+
+    //        if (fittingChars.Count == 0)
+    //        {
+    //            if (allChars.Count <= 1) yield break;
+    //            var charsInLineExceptLast = charsInLine.TakeWhile((c, i) => i < charsInLine.Count - 1).ToList();
+    //            charsInLineExceptLast.ForEach(c => allChars.Dequeue());
+    //            yield return charsInLineExceptLast.Concat(new char[] { '-' });
+    //            continue;
+    //        }
+
+    //        if (fittingChars.Count == charsInLine.Count)
+    //        {
+    //            yield return charsInLine;
+    //            continue;
+    //        }
+
+    //        if (numberOfSpaces > 0)
+    //        {
+    //            int spacesToFill = charsPerLine - fittingChars.Count;
+    //            for (int i = 0; i < spacesToFill; i++)
+    //            {
+    //                fittingChars.Insert(fittingChars.FindNIndex((i+i) % numberOfSpaces, c => c == ' '), ' ');
+    //                numberOfSpaces++;
+    //            }
+
+    //        }
+    //        yield return fittingChars;
+    //    }
+    //}
+
+    public static int FindNIndex<T>(this IEnumerable<T> source, int n, Predicate<T> match)
+    {
+        if (n < 0) return -1;
+        if (match == null) throw new ArgumentNullException("match cannot be null.");
+
+        int index = 0;
+        var enumerator = source.GetEnumerator();
+        while (enumerator.MoveNext())
+        {
+            var current = enumerator.Current;
+            if (match(current))
+            {
+                n--;
+                if (n < 0)
+                {
+                    return index;
+                }
+            }
+            index++;
+        }
+        return -1;
+    }
+}
--- Train/Assets/Scripts/Gameplay/UI/TextComponent.cs
+++ Train/Assets/Scripts/Gameplay/UI/TextComponent.cs
@@ -4,27 +4,34 @@ using UnityEngine;
 
 public class TextComponent : MonoBehaviour
 {
-    private string innerText;
     public string Text;
     public int Layer;
     public Color TextureColor;
     public bool Inverted;
     public bool RightToLeft;
     public bool InvertText;
-
-    private bool _initialized;
-
+    public float CharDistance;
+    public bool Translate;
     public Vector2 Offset;
     public Vector2 Scale;
+    //public int CharactersPerLine;
+    public TextDisposition Disposition;
 
     private float _space = Constants.Distances.TextSpaceDistance;
     private GameManager gameManager;
-
     private Sprite[] fontSpritesArray;
     private Dictionary<char, Sprite> fontSprites;
-    public float CharDistance;
+    private bool _initialized;
+    private string innerText;
+    private int charactersPerLine;
+    private RectTransform rectTransform;
 
-    public bool Translate;
+    public enum TextDisposition
+    {
+        SingleLine,
+        MultiLine,
+        MultiLineHyphenated
+    }
 
     private void Awake()
     {
@@ -34,6 +41,14 @@ public class TextComponent : MonoBehaviour
 
     private void Start()
     {
+        this.rectTransform = this.GetComponent<RectTransform>();
+        this.charactersPerLine = int.MaxValue;
+        if (rectTransform != null)
+        {
+            this.charactersPerLine = (int)this.rectTransform.sizeDelta.x / (int)(this.fontSprites.First().Value.bounds.size.x * (this.Scale.x > 0 ? this.Scale.x : 1)); 
+            if (this.charactersPerLine == 0) this.charactersPerLine = 1;
+        }
+
         this.gameManager = GameManager.GetGameManager();
         if (!_initialized)
         {
@@ -46,7 +61,6 @@ public class TextComponent : MonoBehaviour
         this.innerText = Text;
     }
 
-    // Update is called once per frame
     private void Update()
     {
         if (innerText != Text)
@@ -75,29 +89,75 @@ public class TextComponent : MonoBehaviour
         {
             Destroy(glyph.gameObject);
         }
-        float offset = 0;
-        var charList = (InvertText ? this.Text.ToUpper().Reverse() : this.Text.ToUpper());
-        foreach (char c in charList)
+
+        var charList = (InvertText ? this.Text.ToUpper().Reverse() : this.Text.ToUpper()).ToArray();
+
+        switch (this.Disposition)
         {
+            case TextDisposition.SingleLine:
+                SetTextSingleLine(charList);
+                break;
+            case TextDisposition.MultiLine:
+                SetTextMultiLine(charList);
+                break;
+            case TextDisposition.MultiLineHyphenated:
+                SetTextMultiLineHyphenated(charList);
+                break;
+        }
+    }
+
+    private void SetTextSingleLine(char[] text, int lineOffset=0)
+    {
+        Vector2 offset = Vector2.zero;
+        for (int i = 0; i < text.Length; i++)
+        {
+            char c = text[i];
             if (c == ' ')
             {
-                offset += _space * (this.Scale.x > 0 ? this.Scale.x : 1) * (RightToLeft ? -1 : 1);
                 continue;
             }
 
-            GameObject glyph = new GameObject("Glyph_" + c.ToString());
-            glyph.transform.parent = this.transform;
-            glyph.transform.localScale = this.Scale == Vector2.zero ? Vector3.one : new Vector3(this.Scale.x, this.Scale.y);
-
-            SpriteRenderer renderer = glyph.AddComponent<SpriteRenderer>();
-            renderer.sprite = fontSprites[c];
-            renderer.transform.parent = glyph.transform;
-            renderer.transform.localPosition = Vector3.zero + new Vector3(this.Offset.x + offset, this.Offset.y, 0);
-            renderer.sortingOrder = this.Layer;
-            renderer.material.SetColor("_Color", this.TextureColor);
-
-            offset += (renderer.sprite.rect.size.x + this.CharDistance) * (this.Scale.x > 0 ? this.Scale.x : 1) * (RightToLeft ? -1 : 1);
+            SpriteRenderer renderer = CreateGlyph(c);
+            offset.x = (i * renderer.sprite.rect.size.x + this.CharDistance) * (this.Scale.x > 0 ? this.Scale.x : 1) * (RightToLeft ? -1 : 1);
+            offset.y = lineOffset * -renderer.sprite.rect.size.y * (this.Scale.x > 0 ? this.Scale.x : 1);
+            renderer.transform.localPosition = Vector3.zero + new Vector3(this.Offset.x + offset.x, this.Offset.y + offset.y, 0);
         }
+    }
+
+    private void SetTextMultiLine(char[] text)
+    {
+        char[][] lines = text.GroupByWords(this.charactersPerLine).Select(h => h.ToArray()).ToArray();
+        int lineNumber = 0;
+        foreach (char[] line in lines)
+        {
+            SetTextSingleLine(line, lineNumber);
+            lineNumber++;
+        }
+    }
+
+    private void SetTextMultiLineHyphenated(char[] text)
+    {
+        char[][] lines = text.HyphenateAllLines(this.charactersPerLine).Select(h => h.ToArray()).ToArray();
+        int lineNumber = 0;
+        foreach(char[] line in lines)
+        {
+            SetTextSingleLine(line, lineNumber);
+            lineNumber++;
+        }
+    }
+
+    private SpriteRenderer CreateGlyph(char c)
+    {
+        GameObject glyph = new GameObject("Glyph_" + c.ToString());
+        glyph.transform.parent = this.transform;
+        glyph.transform.localScale = this.Scale == Vector2.zero ? Vector3.one : new Vector3(this.Scale.x, this.Scale.y);
+
+        SpriteRenderer renderer = glyph.AddComponent<SpriteRenderer>();
+        renderer.sprite = fontSprites[c];
+        renderer.transform.parent = glyph.transform;
+        renderer.sortingOrder = this.Layer;
+        renderer.material.SetColor("_Color", this.TextureColor);
+        return renderer;
     }
 
     private char GetSymbolTranslation(string symbol)
